72d95570ff296be2aaecc064e32b43f7f850ae5d
enricotj <taiphoon007@gmail.com>
2018-08-14 09:49:02 -0700

    Removing old firebase creds, add back keys.jsx

--- web/src/config/dev.jsx
+++
@@ -1,5 +0,0 @@
-export const FirebaseConfig = {
-	apiKey: "AIzaSyBpmpT74KPsoY4tlVnGP1j9lyM_JmQ5AF0",
-	authDomain: "lifedash-ec268.firebaseapp.com",
-	databaseURL: "https://lifedash-ec268.firebaseio.com"
-};
